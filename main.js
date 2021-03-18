799fb172be515d5b5f2fdf1276e8e5ad9cffbf4b
y-solb <ysolb99@gmail.com>
2021-03-18 16:42:46 +0900

    JavaScript:타이머 출력,정지 버튼 생성

--- main.js
+++ main.js
@@ -1,16 +1,88 @@
 'use strict';
 
 const CARROT_SIZE = 80;
-const playBtn = document.querySelector('.play__btn');
+const CARROT_COUNT = 5;
+const BUG_COUNT = 5;
+const GAME_DURATION_SEC = 5;
+
 const field = document.querySelector('.game__field');
 const fieldRect = field.getBoundingClientRect();
+const gameBtn = document.querySelector('.game__btn');
+const timeBox = document.querySelector('.time__box');
+const scoreBox = document.querySelector('.score__box');
+const popUp = document.querySelector('.pop-up');
 
-playBtn.addEventListener('click', () => {
-  initGame();
+let started = false;
+let score = 0;
+let timer = undefined;
+
+gameBtn.addEventListener('click', () => {
+  if (started) {
+    stopGame();
+  } else {
+    startGame();
+  }
+  started = !started;
+  console.log(started);
 });
 
+function startGame() {
+  initGame();
+  showStopBtn();
+  showTimerAndScore();
+  startTimer();
+}
+
+function startTimer() {
+  let remainingTimeSec = GAME_DURATION_SEC;
+  updateTimerText(remainingTimeSec);
+  timer = setInterval(() => {
+    if (remainingTimeSec <= 0) {
+      clearInterval(timer);
+      return;
+    }
+    updateTimerText(--remainingTimeSec);
+  }, 1000);
+}
+
+function updateTimerText(time) {
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  timeBox.innerText = `${minutes}:${seconds}`;
+}
+
+function stopGame() {
+  stopTimer();
+  hideGameBtn();
+  showPopup();
+}
+
+function stopTimer() {
+  clearInterval(timer);
+}
+
+function hideGameBtn() {
+  gameBtn.style.visibility = 'hidden';
+}
+
+function showPopup() {
+  popUp.classList.remove('pop-up__hide');
+}
+
+function showStopBtn() {
+  const icon = gameBtn.querySelector('.fa-play');
+  icon.classList.add('fa-stop');
+  icon.classList.remove('fa-play');
+}
+
+function showTimerAndScore() {
+  timeBox.style.visibility = 'visible';
+  scoreBox.style.visibility = 'visible';
+}
+
 function initGame() {
-  console.log(fieldRect);
+  field.innerHTML = '';
+  scoreBox.innerText = CARROT_COUNT;
   addItems('carrot', 5, 'img/carrot.png');
   addItems('bug', 5, 'img/bug.png');
 }
@@ -20,6 +92,7 @@ function addItems(className, count, imgPath) {
   const y1 = 0;
   const x2 = fieldRect.width - CARROT_SIZE;
   const y2 = fieldRect.height - CARROT_SIZE;
+
   for (let i = 0; i < count; i++) {
     const item = document.createElement('img');
     item.setAttribute('class', className);
