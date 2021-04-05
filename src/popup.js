50360d78f2a1f7f89960d3820cb418daca0cdb2b
y-solb <ysolb99@gmail.com>
2021-04-05 21:31:02 +0900

    JavaScript: 변수명 수정

--- src/popup.js
+++ src/popup.js
@@ -1,27 +1,27 @@
-'use strict';
+"use strict";
 
 export default class PopUp {
   constructor() {
     // 초기화
-    this.popUp = document.querySelector('.pop-up');
-    this.popUpText = document.querySelector('.message__box');
-    this.replayBtn = document.querySelector('.replay__btn'); // 클래스에 멤버변수 3개 만듬
-    this.replayBtn.addEventListener('click', () => {
-      this.onClick && this.onClick();
+    this.popUp = document.querySelector(".pop-up");
+    this.popUpText = document.querySelector(".message__box");
+    this.replayBtn = document.querySelector(".replay__btn"); // 클래스에 멤버변수 3개 만듬
+    this.replayBtn.addEventListener("click", () => {
+      this.onReplayBtnClick && this.onReplayBtnClick();
       this.hide();
     });
   }
 
-  setClickListener(onClick) {
-    this.onClick = onClick;
+  setClickListener(onReplayBtnClick) {
+    this.onReplayBtnClick = onReplayBtnClick;
   }
 
   hide() {
-    this.popUp.classList.add('pop-up__hide');
+    this.popUp.classList.add("pop-up__hide");
   }
 
   showWithText(text) {
     this.popUpText.innerText = text;
-    this.popUp.classList.remove('pop-up__hide');
+    this.popUp.classList.remove("pop-up__hide");
   }
 }
